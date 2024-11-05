624bb9185bcedf6a08e6dfaa1cb662f869beb8f0
Brian Faust <brian@faust.de.com>
2024-11-05 07:39:42 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ src/HasUlid.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Ulid;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * @mixin Model
+ */
+trait HasUlid
+{
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+
+    protected static function bootHasUlid(): void
+    {
+        static::creating(function (Model $model): void {
+            if (!$model->getKey()) {
+                $model->{$model->getKeyName()} = Str::ulid();
+            }
+        });
+    }
+}
